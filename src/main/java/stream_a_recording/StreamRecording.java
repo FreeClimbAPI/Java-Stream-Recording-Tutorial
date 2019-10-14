ed9d289926782ad187f7a55110035be414d02cf2
Francis San Filippo <fsanfilipp@vailsys.com>
2019-10-14 11:05:19 -0500

    Initial Commit

---
+++ src/main/java/stream_a_recording/StreamRecording.java
@@ -0,0 +1,35 @@
+package main.java.stream_a_recording;
+
+import com.vailsys.persephony.api.PersyClient;
+import com.vailsys.persephony.KnownSizeInputStream;
+import com.vailsys.persephony.api.PersyException;
+
+public class StreamRecording {
+
+  public static void main(String[] args) {
+    String accountId = System.getenv("ACCOUNT_ID");
+    String authToken = System.getenv("AUTH_TOKEN");
+    String recordingId = "REc03490954166c76d4fc7c895172c0462932a6c4c";
+
+    streamRecording(recordingId, accountId, authToken);
+  }
+
+  public static void streamRecording(String recordingId, String accountId, String authToken) {
+    PersyClient client;
+    KnownSizeInputStream stream;
+
+    try {
+      // Create PersyClient object
+      client = new PersyClient(accountId, authToken);
+
+      /*
+       * Make the request for the recording. Receiving an InputStream in return which
+       * can be used to stream the recording.
+       */
+      stream = client.recordings.stream(recordingId);
+
+    } catch (PersyException pe) {
+      System.out.println(pe.getMessage());
+    }
+  }
+}
